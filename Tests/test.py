3c0d52814b37132c1cb0b7bad4bcfb83a5ece2cb
jan-sb <jan.sbiegay@pm.me>
2024-01-28 17:51:34 +0000

    first seg tests

--- Tests/test.py
+++ Tests/test.py
@@ -1,5 +1,26 @@
+import torch
+import numpy as np
 import segmentation_models_pytorch as smp
 
-model = smp.Unet()
+
+
+
+ENCODER = 'se_resnext50_32x4d'
+ENCODER_WEIGHTS = 'imagenet'
+CLASSES = ['car']
+ACTIVATION = 'sigmoid' # could be None for logits or 'softmax2d' for multiclass segmentation
+DEVICE = 'cuda'
+
+# create segmentation model with pretrained encoder
+model = smp.FPN(
+    encoder_name=ENCODER, 
+    encoder_weights=ENCODER_WEIGHTS, 
+    classes=len(CLASSES), 
+    activation=ACTIVATION,
+)
+
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+
 
 
---
+++ Tests/test2.py
@@ -0,0 +1,38 @@
+import torch
+from torchvision import models, transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Load the pretrained DeepLabV3 model with a ResNet backbone
+model = models.segmentation.deeplabv3_resnet101()
+model.eval()  # Set the model to evaluation mode (no gradient computation during inference)
+
+# Define the transformation for the input image
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert PIL Image to PyTorch Tensor
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize image
+])
+
+# Load and preprocess the input image
+image_path = "Studienarbeit-CODE_Semantische_Segmentation/testdata/inst_picset/frame_0000.png"
+input_image = Image.open(image_path).convert("RGB")  # Open the image in RGB mode
+input_tensor = transform(input_image)  # Apply the defined transformations
+input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension (single image)
+
+# Perform inference
+with torch.no_grad():
+    output = model(input_batch)['out'][0]  # Forward pass through the model
+output_predictions = output.argmax(0)  # Get the index of the most probable class for each pixel
+
+# Visualize the original image and segmentation result
+plt.figure(figsize=(12, 6))
+# Plot the original image
+plt.subplot(1, 2, 1)
+plt.imshow(input_image)
+plt.title('Original Image')
+
+# Plot the segmentation result
+plt.subplot(1, 2, 2)
+plt.imshow(output_predictions, cmap='viridis')
+plt.title('Semantic Segmentation')
+plt.savefig('Studienarbeit-CODE_Semantische_Segmentation/Ergebnisse/testdata/output.png')
