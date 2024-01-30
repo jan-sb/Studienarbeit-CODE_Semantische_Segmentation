a6a10e4ee513c24a5a59beed8bcff423416577c6
jan-sb <jan.sbiegay@pm.me>
2024-01-30 18:42:37 +0000

    Stand Dienstag:
    Todo: draw_segmentation_mask

--- Tests/test.py
+++ Tests/test.py
@@ -7,11 +7,10 @@ import matplotlib.pyplot as plt
 
 
 
-# create segmentation model with pretrained encoder
 
-model = ts.Unet(
-    encoder_name="mobilenet_v2",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+model = ts.DeepLabV3Plus(
+    encoder_name="resnet101",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_weights= "imagenet",     # use `imagenet` pre-trained weights for encoder initialization
     in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
     classes=21,                      # model output channels (number of classes in your dataset)
 )
@@ -24,7 +23,7 @@ transform = transforms.Compose([
     transforms.Pad(padding=(0,4)),    
 ])
 
-image_path = "Studienarbeit-CODE_Semantische_Segmentation/testdata/inst_picset/frame_1038.png"
+image_path = "Studienarbeit-CODE_Semantische_Segmentation/testdata/inst_picset/frame_1038.png" #1038
 input_image = Image.open(image_path).convert("RGB")  
 input_tensor = transform(input_image)  
 input_batch = input_tensor.unsqueeze(0)  
@@ -33,8 +32,9 @@ input_batch = input_tensor.unsqueeze(0)
 
 
 with torch.no_grad():
-    output = model(input_batch)  
-output_predictions = output.argmax(1)[0] 
+    output = model.forward(input_batch)
+print(output.shape)
+output_predictions = output.argmax(1)
 
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
@@ -44,6 +44,6 @@ plt.title('Original Image')
 
 # Plot the segmentation result
 plt.subplot(1, 2, 2)
-plt.imshow(output_predictions, cmap='viridis')
+plt.imshow(output_predictions[0], cmap='viridis')
 plt.title('Semantic Segmentation')
-plt.savefig(f"Studienarbeit-CODE_Semantische_Segmentation/Ergebnisse/ergdata/UNet_resnet34.png")
+plt.savefig(f"Studienarbeit-CODE_Semantische_Segmentation/Ergebnisse/ergdata/latest.png")
--- Tests/test2.py
+++ Tests/test2.py
@@ -1,41 +1,55 @@
 import torch
-from torchvision import models, transforms
+from torchvision import models, transforms, utils, io
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
+from torchvision.utils import draw_segmentation_masks
 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
 
 weights = 'DEFAULT'
 
 # Load the pretrained DeepLabV3 model with a ResNet backbone
-model = models.segmentation.fcn_resnet50(weights=weights, num_classes= 21)
+model = models.segmentation.deeplabv3_resnet101(weights=weights, num_classes= 21)
 model.eval()  # Set the model to evaluation mode (no gradient computation during inference)
 
-# Define the transformation for the input image
+#Define the transformation for the input image
 transform = transforms.Compose([
     transforms.ToTensor(),  # Convert PIL Image to PyTorch Tensor
+    #transforms.Resize(520, 520),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize image
 ])
 
-# Load and preprocess the input image
-image_path = "Studienarbeit-CODE_Semantische_Segmentation/testdata/inst_picset/frame_1038.png"
+
+#Load and preprocess the input image
+image_path = "Studienarbeit-CODE_Semantische_Segmentation/testdata/inst_picset/frame_0005.png"
 input_image = Image.open(image_path).convert("RGB")  # Open the image in RGB mode
 input_tensor = transform(input_image)  # Apply the defined transformations
 input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension (single image)
 
 # Perform inference
 with torch.no_grad():
-    output = model(input_batch)['out'][0]  # Forward pass through the model
-output_predictions = output.argmax(0)  # Get the index of the most probable class for each pixel
+    output = model(input_batch)['out']  # Forward pass through the model
+print('Shape ', output.shape)
+#output_predictions = output.argmax(0)  # Get the index of the most probable class for each pixel
 
-# Visualize the original image and segmentation result
-plt.figure(figsize=(12, 6))
-# Plot the original image
-plt.subplot(1, 2, 1)
-plt.imshow(input_image)
-plt.title('Original Image')
+om = torch.argmax(output.squeeze(), dim=0).detach().cpu().numpy()
+#print ('OM Shape ', om.shape, ' OM Type ', type(om))
+#print ('Uniques ' , np.unique(om), ' Max ', max(np.unique(om)))
+#print('Unique Number ', len(np.unique(om)))
 
-# Plot the segmentation result
-plt.subplot(1, 2, 2)
-plt.imshow(output_predictions, cmap='viridis')
-plt.title('Semantic Segmentation')
-plt.savefig(f"Studienarbeit-CODE_Semantische_Segmentation/Ergebnisse/testdata/FCN50_{weights}.png")
+
+image = input_image.ToTensor
+
+
+
+
+
+
+plt.imshow(om)
+
+plt.savefig('Studienarbeit-CODE_Semantische_Segmentation/Ergebnisse/ergdata/latest.png')
+
+
