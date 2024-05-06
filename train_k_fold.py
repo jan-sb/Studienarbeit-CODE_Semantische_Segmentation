2e07a55ef1de30de066c20c67b9feaf9283f09d6
jan-sb <jan.sbiegay@pm.me>
2024-05-06 21:59:47 +0200

    save before ove rnight run

--- train_k_fold.py
+++ train_k_fold.py
@@ -23,36 +23,42 @@ total_eppochs = 15
 epoch_steps = 1
 runs = total_eppochs // epoch_steps
 
-for model in deeplv3:
-    for i in range(5):
-        i = 0
-        create_model_directory(model, i)
-        trained_model = TrainedModel(model, 2048, 1024, f'{model}_k_fold_{i}', start_epoch='latest')
-        k_fold_dataset = K_Fold_Dataset(image_dir='CityscapesDaten/images',
-                                        annotation_dir='CityscapesDaten/semantic',
-                                        k_fold_csv_dir='Daten/CityscapesDaten',
-                                        leave_out_fold=i)        
-        
-        k_fold_dataset.check_for_data_leaks()               
-        
-        trained_model.prepare_model_training(dataset_train=k_fold_dataset.train_dataset,
-                                            dataset_val=k_fold_dataset.val_dataset,
-                                            batch_size=2, 
-                                            shuffle=True, 
-                                            learning_rate=1*10**(-7), 
-                                            momentum=0.9,
-                                            weight_decay=0.00001)
-    
-        
-        trained_model.auto_train(epochs=total_eppochs, max_deviations=5)
-        #trained_model.train(1)
-        
-        # image, annotation = k_fold_dataset.train_dataset[0]
-        
-        # visualize_image_and_annotation(image, annotation)
-        sys.exit()
-             
+
+
+for model in all_models:
+    try: 
+        for i in range(5):
+            i = 0
+            create_model_directory(model, i)
+            trained_model = TrainedModel(model, 2048, 1024, f'{model}_k_fold_{i}', start_epoch='latest')
+            k_fold_dataset = K_Fold_Dataset(image_dir='CityscapesDaten/images',
+                                            annotation_dir='CityscapesDaten/semantic',
+                                            k_fold_csv_dir='Daten/CityscapesDaten',
+                                            leave_out_fold=i)        
             
-        #trained_model.writer.flush()
-        #trained_model.writer.close()
+            k_fold_dataset.check_for_data_leaks()               
+            
+            trained_model.prepare_model_training(dataset_train=k_fold_dataset.train_dataset,
+                                                dataset_val=k_fold_dataset.val_dataset,
+                                                batch_size=2, 
+                                                shuffle=True, 
+                                                learning_rate=1*10**(-7), 
+                                                momentum=0.9,
+                                                weight_decay=0.00001)
         
+            
+            trained_model.auto_train(epochs=total_eppochs, max_deviations=5)
+            #trained_model.train(1)
+            
+            # image, annotation = k_fold_dataset.train_dataset[0]
+            
+            # visualize_image_and_annotation(image, annotation)
+            
+                
+            trained_model.writer.flush()
+            trained_model.writer.close()
+    except Exception as e:
+        error_message = str(e)
+        with open('error.json', 'w') as f:
+            json.dump({'error': error_message}, f)
+        os.system('sudo shutdown now -h')
