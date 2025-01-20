b127e4445f7f561009bfde3f472fa93608361024
jan-sb <jan.sbiegay@pm.me>
2025-01-20 10:34:49 +0100

    Hyperparameter Run V2.0 lraspp_mobilenet_v3_large

--- Hyperparameter_Tuning.py
+++ Hyperparameter_Tuning.py
@@ -45,7 +45,7 @@ k_fold_dataset = K_Fold_Dataset(
 )
 k_fold_dataset.check_for_data_leaks()         
 
-model = all_models[2]      
+model = all_models[3]      
 
 def train_hyper(config, checkpoint_dir=None):  
     try:
@@ -187,7 +187,7 @@ tuner = tune.Tuner(
     run_config=train.RunConfig(
         name="Hyperparameter_Tuning_Deeplabv3",
         # Wichtig: Speichert alles im storage_path
-        storage_path="/home/jan/studienarbeit/Studienarbeit-CODE_Semantische_Segmentation/HyperparameterLOG/deeplabv3_mobilenet_v3_large",
+        storage_path="/home/jan/studienarbeit/Studienarbeit-CODE_Semantische_Segmentation/HyperparameterLOG/lraspp_mobilenet_v3_large",
         checkpoint_config=CheckpointConfig(
             num_to_keep=3,
             checkpoint_score_attribute="val_loss",
